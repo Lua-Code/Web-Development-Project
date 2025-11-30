8573f33356060645bc3ce34a1e4d07a694e9cfba
Kevin David <kevindavidvenyk@gmail.com>
2025-11-30 23:30:10 +0200

    Update MainLayout.jsx

--- seller-app/src/Layouts/MainLayout.jsx
+++ seller-app/src/Layouts/MainLayout.jsx
@@ -2,14 +2,19 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 
 const MainLayout = () => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-1">
-        <Outlet />
-      </main>
-    </div>
-  );
+    const handleLogout = () => {
+        /* authentication logic later ba2a ie. clearing el auth token etc*/
+
+        window.location.href = 'http://localhost:5173/'; /* should be replaced by za buyer URL */
+    }
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Navbar onLogout = {handleLogout} />
+            <main className="flex-1">
+                <Outlet />
+            </main>
+        </div>
+    );
 };
 
 export default MainLayout;
