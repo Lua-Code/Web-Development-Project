e98475b4fce093855d40083bf7098f9eaf03cda6
Kevin David <kevindavidvenyk@gmail.com>
2025-12-07 21:26:12 +0200

    Update RevenueByCategoryItem.jsx

--- seller-app/src/Components/Analytics/RevenueByCategoryItem.jsx
+++ seller-app/src/Components/Analytics/RevenueByCategoryItem.jsx
@@ -2,7 +2,7 @@ import ProgressBar from "./ProgressBar";
 
 const RevenueByCategoryItem = ({ category, revenue, percentage }) => {
     return (
-        <div className="bg-white p-4 flex flex-col gap-2 border-b">
+        <div className="bg-white p-4 flex flex-col gap-2">
             <div className="flex justify-between items-center">
                 <p className="font-medium text-[#457b9d]">{category}</p>
                 <span className="font-medium">{revenue} ({percentage}%)</span>
