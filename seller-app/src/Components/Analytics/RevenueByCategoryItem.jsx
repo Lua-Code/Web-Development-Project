5525a3e0da213f7a7e93e9debc7f1e3bd88466dc
Kevin David <kevindavidvenyk@gmail.com>
2025-12-07 21:26:51 +0200

    Update RevenueByCategoryItem.jsx

--- seller-app/src/Components/Analytics/RevenueByCategoryItem.jsx
+++ seller-app/src/Components/Analytics/RevenueByCategoryItem.jsx
@@ -5,7 +5,7 @@ const RevenueByCategoryItem = ({ category, revenue, percentage }) => {
         <div className="bg-white p-4 flex flex-col gap-2">
             <div className="flex justify-between items-center">
                 <p className="font-medium text-[#457b9d]">{category}</p>
-                <span className="font-medium">{revenue} ({percentage}%)</span>
+                <span className="font-medium">${revenue} ({percentage}%)</span>
             </div>
             <ProgressBar percentage={percentage} />
         </div>
