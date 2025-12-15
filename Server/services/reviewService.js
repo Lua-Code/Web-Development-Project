51bd96eb251864943c05825e0fc23b382e1524a5
Kevin David <kevindavidvenyk@gmail.com>
2025-12-15 04:22:36 +0200

    Remove empty service stubs from Server/services
    Deleted placeholder service files for listings, messages, orders, reviews, sellers, transactions, and users. These files contained only empty exported functions and were not implemented.

--- Server/services/reviewService.js
+++
@@ -1,5 +0,0 @@
-exports.getReviews = async (req, res) => {};
-exports.getReviewById = async (req, res) => {};
-exports.createReview = async (req, res) => {};
-exports.updateReview = async (req, res) => {};
-exports.deleteReview = async (req, res) => {};
--- Server/services/transactionService.js
+++
@@ -1,5 +0,0 @@
-exports.getTransactions = async (req, res) => {};
-exports.getTransactionById = async (req, res) => {};
-exports.createTransaction = async (req, res) => {};
-exports.updateTransaction = async (req, res) => {};
-exports.deleteTransaction = async (req, res) => {};
