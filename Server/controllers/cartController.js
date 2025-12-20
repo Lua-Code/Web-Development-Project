66d463aef94a4535481d137f9619f9634acc9e70
Kevin David <kevindavidvenyk@gmail.com>
2025-12-20 09:50:30 +0200

    I WILL KILL MYSELF

--- Server/controllers/cartController.js
+++ Server/controllers/cartController.js
@@ -1,14 +1,16 @@
 import * as cartService from "../services/cartService.js";
 
-export const addToCart = async (req, res) => {
+export const addToCartHandler = async (req, res) => {
   try {
-    console.log("Add to cart request body and userId:", req.body, req.session.userId);
+    const userId = req.session.userId; 
     const { productId, quantity } = req.body;
-    const userId = req.session.userId;
-    console.log("Adding to cart - userId:", userId, "productId:", productId, "quantity:", quantity);
+
+    if (!productId || !quantity) {
+      return res.status(400).json({ message: "productId and quantity are required" });
+    }
 
     const cart = await cartService.addToCart(userId, productId, quantity);
-    res.status(200).json({ message: "Item added to cart", cart });
+    res.status(200).json({ message: "Item added to cart successfully", cart });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
--- Server/routes/cartRoutes.js
+++ Server/routes/cartRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { addToCartHandler } from "../controllers/cartController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
+
 const router = express.Router();
-import { addToCart } from "../controllers/cartController.js";
 
-
-router.post("/",addToCart);
+router.post("/", authMiddleware, addToCartHandler);
 
 export default router;
