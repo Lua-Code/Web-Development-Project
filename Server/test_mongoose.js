301f0dfa8c6df44b194059ffef63b4dab9a03700
Madonna Emad <madonnaemad16@gmail.com>
2025-12-20 02:13:45 +0200

    chore: remove unused test files

--- Server/test_mongoose.js
+++
@@ -1,9 +0,0 @@
-
-import mongoose from "mongoose";
-console.log("Mongoose loaded");
-try {
-    const Listing = (await import("./models/Listing.js")).default;
-    console.log("Listing loaded");
-} catch (e) {
-    console.error("Listing load failed:", e);
-}
